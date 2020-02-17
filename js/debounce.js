8dd9c41c3c92eedb2ba2d03399a67ffaa880db78
Alexandr Timoshenskiy <timoshenskiy.ad@gmail.com>
2020-02-17 23:01:35 +0300

    Исправление недочетов

--- js/debounce.js
+++ js/debounce.js
@@ -1,11 +1,20 @@
 'use strict';
 (function () {
+  var RANDOM_PHOTO_COUNT = 10;
   var DEBOUNCE_INTERVAL = 500;
 
   var lastTimeout;
-  window.debounce = function (cb) {
+  window.debounce = function (target, cb) {
     if (lastTimeout) {
       window.clearTimeout(lastTimeout);
+    }
+    if (target.textContent === 'Случайные') {
+      window.searchFilter.dataCopy = window.util.shuffleArray(window.searchFilter.dataCopy).slice(0, RANDOM_PHOTO_COUNT);
+    }
+    if (target.textContent === 'Обсуждаемые') {
+      window.searchFilter.dataCopy.sort(function (a, b) {
+        return b.comments.length - a.comments.length;
+      });
     }
     lastTimeout = window.setTimeout(cb, DEBOUNCE_INTERVAL);
   };
--- js/search-filter.js
+++ js/search-filter.js
@@ -1,36 +1,27 @@
 'use strict';
 (function () {
-  var RANDOM_PHOTO_COUNT = 10;
-  var photoElementList = document.querySelector('.pictures');
   var filterSection = document.querySelector('.img-filters');
-  var filterButtons = filterSection.querySelectorAll('.img-filters button');
+  var filterButtons = filterSection.querySelectorAll('button');
   var activeButton;
   var onFilterClick = function (evt) {
     var target = evt.target;
-    activeButton = filterSection.querySelector('.img-filters__button--active');
-    activeButton.classList.remove('img-filters__button--active');
-    target.classList.add('img-filters__button--active');
-    activeButton = target;
-    var dataCopy = window.gallery.data.slice();
-    while (photoElementList.firstChild) {
-      photoElementList.firstChild.remove();
-    }
-
-    if (target.textContent === 'Случайные') {
-      dataCopy = window.util.shuffleArray(dataCopy).slice(0, RANDOM_PHOTO_COUNT);
-    }
-    if (target.textContent === 'Обсуждаемые') {
-      dataCopy.sort(function (a, b) {
-        return b.comments.length - a.comments.length;
+    if (!target.classList.contains('img-filters__button--active')) {
+      activeButton = filterSection.querySelector('.img-filters__button--active');
+      activeButton.classList.remove('img-filters__button--active');
+      target.classList.add('img-filters__button--active');
+      activeButton = target;
+      var pictures = document.querySelectorAll('.picture');
+      pictures.forEach(function (it) {
+        it.remove();
+      });
+      window.searchFilter = {
+        dataCopy: window.gallery.data.slice()
+      };
+      window.debounce(target, function () {
+        window.gallery.addElements(window.searchFilter.dataCopy);
       });
 
-
     }
-    window.debounce(function () {
-      window.gallery.addElements(dataCopy);
-    });
-
-
   };
   filterButtons.forEach(function (it) {
     it.addEventListener('click', onFilterClick);
