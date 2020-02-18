dc91eae696027ecd51822ce5c491d700c39a785e
Alexandr Timoshenskiy <timoshenskiy.ad@gmail.com>
2020-02-19 00:23:54 +0300

    Удаление обработчика

--- js/download-more.js
+++ js/download-more.js
@@ -2,6 +2,23 @@
 (function () {
   var COMMENT_COUNT_STEP = 5;
   var commentLoader = document.querySelector('.comments-loader');
+  var onCommentLoaderClick = function () {
+    var comments = document.querySelectorAll('.social__comment');
+    var condition;
+    if ((window.downloadMore.currentCommentCount + COMMENT_COUNT_STEP) < window.downloadMore.totalCommentCount) {
+      condition = window.downloadMore.currentCommentCount + COMMENT_COUNT_STEP;
+    } else {
+      condition = window.downloadMore.totalCommentCount;
+      commentLoader.classList.add('hidden');
+      commentLoader.removeEventListener('click', onCommentLoaderClick);
+    }
+    for (var i = window.downloadMore.currentCommentCount; i < condition; i++) {
+      comments[i].removeAttribute('style');
+
+    }
+
+    window.downloadMore.currentCommentCount += COMMENT_COUNT_STEP;
+  };
   window.downloadMore = {
     hideComments: function () {
       var comments = document.querySelectorAll('.social__comment');
@@ -14,32 +31,12 @@
       } else {
         window.downloadMore.currentCommentCount = comments.length;
       }
-
-
+      commentLoader.removeEventListener('click', onCommentLoaderClick);
     },
     showComments: function () {
       var comments = document.querySelectorAll('.social__comment');
       // Сохраним количество комментариев у выбранной фотографии
       window.downloadMore.totalCommentCount = comments.length;
-      var onCommentLoaderClick = function () {
-        var condition;
-        if ((window.downloadMore.currentCommentCount + COMMENT_COUNT_STEP) < window.downloadMore.totalCommentCount) {
-          condition = window.downloadMore.currentCommentCount + COMMENT_COUNT_STEP;
-        } else {
-          condition = window.downloadMore.totalCommentCount;
-          commentLoader.classList.add('hidden');
-          commentLoader.removeEventListener('click', onCommentLoaderClick);
-          comments.forEach(function (it) {
-            it.removeAttribute('style');
-          });
-        }
-        for (var i = window.downloadMore.currentCommentCount; i < condition; i++) {
-          comments[i].removeAttribute('style');
-
-        }
-
-        window.downloadMore.currentCommentCount += COMMENT_COUNT_STEP;
-      };
       commentLoader.addEventListener('click', onCommentLoaderClick);
 
     }
